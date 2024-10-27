6ae7b297842cb1d3890e7f0a6b56492641ab96e1
RafaelRaiser <rafabarrosraiser@gmail.com>
2024-10-27 17:38:29 -0300

    Primeira parte

---
+++ TowerDefense/Assets/Scripts/Enemy/EnemyMovemant.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D enemyRigidbody;  // Componente para aplicar a física no inimigo.
+    [SerializeField] private float movementSpeed = 2f;    // Velocidade atual do movimento.
+
+    private Transform currentTarget;   // Alvo atual para onde o inimigo está indo.
+    private int currentPathIndex = 0;  // Índice do caminho atual do inimigo.
+    private float defaultSpeed;        // Armazena a velocidade padrão do inimigo para ser resetada.
+
+    private void Start()
+    {
+        InitializeMovementSettings();
+    }
+
+    private void InitializeMovementSettings()
+    {
+        defaultSpeed = movementSpeed;  // Define a velocidade padrão inicial.
+        currentTarget = LevelManager.instance.path[currentPathIndex]; // Primeiro ponto do caminho como alvo.
+    }
+
+    private void Update()
+    {
+        CheckProximityToTarget();
+    }
+
+    private void CheckProximityToTarget()
+    {
+        if (Vector2.Distance(transform.position, currentTarget.position) <= 0.1f)
+        {
+            SetNextTarget();
+        }
+    }
+}
